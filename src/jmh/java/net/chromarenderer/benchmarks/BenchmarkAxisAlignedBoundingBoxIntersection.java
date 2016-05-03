7ddd86958d42d552d36f6665b3c6ebf9b935a2d7
bensteinert <benjamin.steinert@googlemail.com>
2016-05-03 19:36:58 +0200

    Clarifies jmh source set relation. Updates submodule ref. Extends benchmarks

---
+++ src/jmh/java/net/chromarenderer/benchmarks/BenchmarkAxisAlignedBoundingBoxIntersection.java
@@ -0,0 +1,41 @@
+package net.chromarenderer.benchmarks;
+
+import net.chromarenderer.math.ImmutableVector3;
+import net.chromarenderer.math.Vector3;
+import net.chromarenderer.math.raytracing.Ray;
+import net.chromarenderer.renderer.scene.acc.AxisAlignedBoundingBox;
+import net.chromarenderer.renderer.scene.acc.IntersectionContext;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author bensteinert
+ */
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@BenchmarkMode(Mode.SingleShotTime)
+@Fork(value = 1)
+@Measurement(batchSize = 1000000, iterations = 100)
+public class BenchmarkAxisAlignedBoundingBoxIntersection {
+
+    private static final AxisAlignedBoundingBox bBox = new AxisAlignedBoundingBox(Vector3.ORIGIN, Vector3.ONE);
+    private static Ray ray = new Ray(new ImmutableVector3(0.5f, 0.5f, 2.f), new ImmutableVector3(0.f, 0.f, -1.f));
+    private static IntersectionContext ctx = new IntersectionContext();
+
+    static {
+        ray = new Ray(new ImmutableVector3(0.5f, 0.5f, 2.f), new ImmutableVector3(0.f, 0.f, -1.f));
+        ctx = new IntersectionContext();
+        ctx.reinit(ray);
+    }
+
+    @Benchmark
+    public int benchmarkAABBIntersect() {
+        return bBox.intersects(ctx);
+    }
+
+}
--- src/jmh/java/net/chromarenderer/benchmarks/MoellerTrumboreTriangleIntersectionBenchmark.java
+++ src/jmh/java/net/chromarenderer/benchmarks/MoellerTrumboreTriangleIntersectionBenchmark.java
@@ -1,7 +1,8 @@
 package net.chromarenderer.benchmarks;
 
 import net.chromarenderer.math.ImmutableVector3;
-import net.chromarenderer.math.geometry.Triangle;
+import net.chromarenderer.math.geometry.ObjectLayoutTriangle;
+import net.chromarenderer.math.geometry.SimpleTriangle;
 import net.chromarenderer.math.raytracing.Ray;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
@@ -13,23 +14,35 @@ import org.openjdk.jmh.annotations.OutputTimeUnit;
 import java.util.concurrent.TimeUnit;
 
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
+@BenchmarkMode(Mode.SingleShotTime)
+@Fork(value = 1)
+@Measurement(batchSize = 1000000, iterations = 100)
 public class MoellerTrumboreTriangleIntersectionBenchmark {
 
-
-    private static Triangle TRIANGLE = new Triangle(
+    private static SimpleTriangle TRIANGLE = new SimpleTriangle(
             new ImmutableVector3(0.f, 0.f, 1.f),    //x
             new ImmutableVector3(.0f, 1.f, 1.f),    //y
             new ImmutableVector3(1.f, 0.f, 1.f),    //z
             new ImmutableVector3(0.f, 0.f, -1.f));
+
+    private static ObjectLayoutTriangle OBJECT_TRIANGLE = ObjectLayoutTriangle.createTriangle(
+            new ImmutableVector3(0.f, 0.f, 1.f),    //x
+            new ImmutableVector3(.0f, 1.f, 1.f),    //y
+            new ImmutableVector3(1.f, 0.f, 1.f),    //z
+            new ImmutableVector3(0.f, 0.f, -1.f));
+
     private static Ray RAY = new Ray(new ImmutableVector3(0.2f, 0.2f, 0.f), new ImmutableVector3(0.f, 0.f, 1.f));
 
 
     @Benchmark
-    @BenchmarkMode(Mode.SingleShotTime)
-    @Fork(value = 1)
-    @Measurement(batchSize = 1000000, iterations = 100)
     public float benchmarkMoellerTrumboreTriangleIntersect() {
         return TRIANGLE.intersect(RAY);
+    }
+
+
+    @Benchmark
+    public float benchmarkMoellerTrumboreObjectLayoutTriangleIntersect() {
+        return OBJECT_TRIANGLE.intersect(RAY);
     }
 
 }
